174b978888763cd6d75d7a97c3c1640a28b991d0
rrehamosama@gmail.com <rrehamosama4@gmail.com>
2025-07-24 15:06:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let s='*'
+for(let i=0;i<6;i++){
+    console.log(s);
+    s=s+'*'
+}
+for (let i = 7; i > 0; i--) {
+    let s = "";
+    for (let j = 0; j < i; j++) {
+        s += "*";
+    }
+console.log(s);
+}
+
